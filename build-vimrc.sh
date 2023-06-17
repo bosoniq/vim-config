7b0d9c91894cb73292031bebd6d436ac7a423a74
Andrew Pitt <bosoniq@gmail.com>
2023-06-17 22:51:14 +0200

    Build script removes current config and symbolic links

--- build-vimrc.sh
+++ build-vimrc.sh
@@ -1,31 +1,35 @@
 #!/usr/bin/env bash
 
 # read/write options
-BUILD=./build
-CONFIGS=./config
 LUANAMESPACE=als56gh
+
+# build directories and files
+BUILD="${PWD}/build"
+CONFIGS="${PWD}/config"
 INIT=${BUILD}/init.vim
+LUAMAIN="${BUILD}//lua//main.lua"
 VIMRC=${BUILD}/vimrc
 COCSETTINGS=${BUILD}/coc-settings.json
 LUAMODULES=${BUILD}/lua/${LUANAMESPACE}/
 
-# prepare vimrc for writing
-if [ ! -d "$BUILD" ]; then
-    mkdir ${BUILD}
-    touch ${VIMRC}
-    touch ${COCSETTINGS}
-    mkdir "${BUILD}//lua/"
-    touch "${BUILD}//lua//main.lua"
-    mkdir ${LUAMODULES}
-else 
-    if [ -f "$VIMRC" ]; then
-        rm ${VIMRC}
-        rm ${COCSETTINGS}
-    fi
+# clean existing build
+if [ -d "$BUILD" ]; then
+    echo -e "\nRemoving symbolic links ...."
+    rm -rv ~/.config/nvim/*
 
-    touch ${VIMRC}
-    touch ${COCSETTINGS}
+    echo -e "\nRemoving current build ...."
+    rm -rv ${BUILD}/*
+    rmdir ${BUILD}
 fi
+
+# create build docs and directories
+mkdir ${BUILD}
+mkdir "${BUILD}//lua/"
+mkdir ${LUAMODULES}
+touch ${VIMRC}
+touch ${COCSETTINGS}
+touch ${INIT}
+touch ${LUAMAIN}
 
 # concat config file contents to build/vimrc
 for CONFIG in ${CONFIGS}/*; do less $CONFIG >> $VIMRC; done
@@ -34,34 +38,37 @@ for CONFIG in ${CONFIGS}/*; do less $CONFIG >> $VIMRC; done
 initVim=$(<init.vim)
 initVim="${initVim//CONFIGDIR/$PWD}"
 echo "$initVim" > ${INIT}
-ln -s ${PWD}/${INIT} ~/.config/nvim/init.vim
 
 # create coc settings file
 cocSettings=$(<coc-settings.json)
 cocSettings="${cocSettings//CONFIGDIR/$PWD}"
 echo "$cocSettings" > ${COCSETTINGS}
-ln -s ${PWD}/${COCSETTINGS} ~/.config/nvim/coc-settings.json
 
 # copy and update lua modules
 main=$(<lua/main.lua)
 main="${main//NAMESPACE/$LUANAMESPACE}"
 echo "$main" >> $VIMRC
 cp -R ${PWD}/lua/modules/. ${LUAMODULES}
-ln -s ${PWD}/${BUILD}/lua ~/.config/nvim/lua
+
+# create symbolic links
+echo -e "\nCreating symbolic links ...."
+ln -s ${INIT} ~/.config/nvim/init.vim
+ln -s ${COCSETTINGS} ~/.config/nvim/coc-settings.json
+ln -s ${BUILD}/lua ~/.config/nvim/lua
 
 # composer dependency installation/update
 composer -v > /dev/null 2>&1
 COMPOSER=$?
 if [[ $COMPOSER -ne 0 ]]; then
-    echo 'Composer is not installed'
+    echo -e "\nComposer is not installed"
 else
 
     if [ -d "./vendor" ] 
     then
-        echo 'Updating composer libraries ....'
+        echo -e "\nUpdating composer libraries ...."
         composer update
     else
-        echo 'Installing composer libraries ....'
+        echo -e "\nInstalling composer libraries ...."
         composer install
     fi
 fi
