741e8fb8a7c33301fc324707e5f7f402c7a9b8af
Andrew Pitt <bosoniq@gmail.com>
2024-03-02 23:52:44 +0100

    Phpunit added for codelens

--- lua/modules/coc.lua
+++ lua/modules/coc.lua
@@ -95,3 +95,5 @@ vim.keymap.set("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<
 vim.keymap.set("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
 -- cycle options upwards
 vim.keymap.set("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+
+vim.keymap.set('n', '<leader>gl', '<Plug>(coc-codelens-action)', {silent = true})
