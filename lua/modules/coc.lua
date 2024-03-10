5813055e24e9ae6ccd02d81ee40f13a10b4c59ad
Andrew Pitt <bosoniq@gmail.com>
2024-03-10 19:50:58 +0100

    Tabbing fix

--- lua/modules/coc.lua
+++ lua/modules/coc.lua
@@ -1,3 +1,25 @@
+-- Function declarations -----------------------------------------
+--
+-- Autocomplete
+function _G.check_back_space()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
+-- Use K to show documentation in preview window
+function _G.show_docs()
+    local cw = vim.fn.expand('<cword>')
+    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+        vim.api.nvim_command('h ' .. cw)
+    elseif vim.api.nvim_eval('coc#rpc#ready()') then
+        vim.fn.CocActionAsync('doHover')
+    else
+        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+    end
+end
+
+------------------------------------------------------------------
+
 vim.opt.updatetime = 1000
 
 vim.g.coc_global_extensions = {
@@ -19,17 +41,6 @@ vim.keymap.set('n', 'gy', '<Plug>(coc-type-definition)', {silent = true})
 vim.keymap.set('n', 'gi', '<Plug>(coc-implementation)', {silent = true})
 vim.keymap.set('n', 'gr', '<Plug>(coc-references)', {silent = true})
 
--- Use K to show documentation in preview window
-function _G.show_docs()
-    local cw = vim.fn.expand('<cword>')
-    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
-        vim.api.nvim_command('h ' .. cw)
-    elseif vim.api.nvim_eval('coc#rpc#ready()') then
-        vim.fn.CocActionAsync('doHover')
-    else
-        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
-    end
-end
 vim.keymap.set("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
 
 
