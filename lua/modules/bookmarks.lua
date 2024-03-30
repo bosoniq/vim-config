9c43fc0cbfae0e23600f936cbf0c046d0947e21b
Andrew Pitt <bosoniq@gmail.com>
2024-03-30 14:11:08 +0100

    Harpoon and bookmarks key combo consolidation

--- lua/modules/bookmarks.lua
+++ lua/modules/bookmarks.lua
@@ -10,11 +10,11 @@ require('bookmarks').setup{
   on_attach = function(bufnr)
     local bm = require "bookmarks"
     local map = vim.keymap.set
-    map("n","<leader>mm",bm.bookmark_toggle) -- add or remove bookmark at current line
-    map("n","<leader>mi",bm.bookmark_ann) -- add or edit mark annotation at current line
-    map("n","<leader>mc",bm.bookmark_clean) -- clean all marks in local buffer
-    map("n","<leader>mn",bm.bookmark_next) -- jump to next mark in local buffer
-    map("n","<leader>mp",bm.bookmark_prev) -- jump to previous mark in local buffer
-    map("n","<leader>ml",bm.bookmark_list) -- show marked file list in quickfix window
+    -- map("n","<leader>mm",bm.bookmark_toggle) -- add or remove bookmark at current line
+    map("n","<leader>bm",bm.bookmark_ann) -- add or edit mark annotation at current line
+    map("n","<leader>bl",bm.bookmark_list) -- show marked file list in quickfix window
+    map("n","<leader>bn",bm.bookmark_next) -- jump to next mark in local buffer
+    map("n","<leader>bp",bm.bookmark_prev) -- jump to previous mark in local buffer
+    map("n","<leader>bc",bm.bookmark_clean) -- clean all marks in local buffer
   end
 }
