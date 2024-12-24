4b8649193d9154060cdbb43836c5a174d61e5f2e
Andrew Pitt <bosoniq@gmail.com>
2024-12-24 18:11:19 +0100

    Telescope cmd search plugin added

--- lua/modules/telescope.lua
+++ lua/modules/telescope.lua
@@ -6,6 +6,7 @@ telescope.setup{}
 telescope.load_extension('recent_files')
 telescope.load_extension('harpoon')
 telescope.load_extension('bookmarks')
+telescope.load_extension('cmdline')
 
 -- Map a shortcut to open the picker.
 vim.api.nvim_set_keymap("n", "<leader>fr",
@@ -16,6 +17,7 @@ vim.api.nvim_set_keymap("n", "<leader>fr",
 vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
 vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
 vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
+vim.keymap.set('n', '<leader>fc', '<cmd>Telescope cmdline<cr>')
 
 -- Disable folding in Telescope's results preview window.
 vim.api.nvim_create_autocmd("User", { pattern = "TelescopePreviewerLoaded", command = [[setlocal nofoldenable]] })
