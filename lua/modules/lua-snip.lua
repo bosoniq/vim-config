3b6d8870ad5d16e4f2e6d5ec7dd81589bff04354
Andrew Pitt <bosoniq@gmail.com>
2024-12-29 14:18:22 +0100

    Snippet updates

--- lua/modules/lua-snip.lua
+++ lua/modules/lua-snip.lua
@@ -2,26 +2,11 @@ local ls = require("luasnip")
 
 -- Config
 vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
-vim.keymap.set({"i", "s"}, "<TAB>", function() ls.jump( 1) end, {silent = true})
-vim.keymap.set({"i", "s"}, "<S-TAB>", function() ls.jump(-1) end, {silent = true})
---
--- vim.keymap.set({"i", "s"}, "<C-E>", function()
--- 	if ls.choice_active() then
--- 		ls.change_choice(1)
--- 	end
--- end, {silent = true})
-
-
 
 -- functions
 local function renderTypeHint(args)
     return string.gsub(args[1][1], "^%l", string.upper)
 end
-
-local function copy(args)
-	return args[1]
-end
-
 
 -- Snippet definition
 local s = ls.snippet
@@ -49,9 +34,8 @@ local fmt = require("luasnip.extras.fmt").fmt
 ls.add_snippets("php", {
     s("dd", fmt("dd(${});", { i(1, "test") })),
     s("shout", fmt("echo '{}!'; exit;", { i(1, "here") })),
-    s("t", fmt("$this->{}({} ${});", { i(1, "method"), i(2, "hint"), i(3, "var") })),
-    s("v", fmt("${} = {};", { i(1, "varName"), i(2, "value") })),
-    s("p", fmt("private {} ${} = {};", { i(1, "type"), i(2, "varName"), i(3, "value") })),
+    s({trig = "vd", name = "var dec"}, fmt("${} = {};", { i(1, "varName"), i(2, "value") })),
+    s({trig = "pd", name = "prop dec"}, fmt("private {} ${} = {};", { i(1, "type"), i(2, "varName"), i(3, "value") })),
 
     -- doc snippets
     s("docTodo", fmt("// TODO: {}", { i(1, "message") })),
@@ -69,6 +53,7 @@ ls.add_snippets("php", {
     s("foreach", fmt(
         [[
             foreach(${} as ${} => ${}) {{
+                {}
             }}
 
         ]],
@@ -76,80 +61,90 @@ ls.add_snippets("php", {
             i(1, "array"),
             i(2, "key"),
             i(3, "value"),
+            i(0),
         }
     )),
     s("if", fmt(
         [[
             if({}) {{
+                {}
             }}
 
         ]],
         {
             i(1, "condition"),
+            i(0),
         }
     )),
-    s("m", fmt(
-        [[
-            public function {}(): {}
-            {{
-                return{};
-            }}
-        ]],
+    s({trig = "mc", desc = "method call"}, fmt("$this->{}({} ${});", { i(1, "method"), i(2, "hint"), i(3, "var") })),
+    s({trig = "m", name = "New method", desc = "Create a new class method"},
+      fmt(
+          [[
+              public function {}(): {}
+              {{
+                  return{};
+              }}
+          ]],
+          {
+              i(1, "NewName"),
+              i(2, "string"),
+              i(3, " ''"),
+          }
+      )
+    ),
+    s({trig = "c", name = "New construct", desc = "Create a new class construct"},
         {
-            i(1, "NewName"),
-            i(2, "string"),
-            i(3, " ''"),
+            t({ "", "public function __construct(private readonly " }),
+            f(renderTypeHint, 1),
+            t(" $"),
+            i(1, "paramName"),
+            t({ ")" }),
+            t({ "", "{" }),
+            t({ "", "}" }),
         }
-    )),
-
-	s("c", {
-		t({ "", "public function __construct(private readonly " }),
-		f(renderTypeHint, 1),
-        t(" $"),
-        i(1, "paramName"),
-		t({ ")" }),
-		t({ "", "{" }),
-		t({ "", "}" }),
-	}),
-
-    s("i", fmt(
-        [[
-            public function __invoke({} ${})
-            {{
-            }}
-        ]],
-        {
-            i(1, "hint"),
-            i(1, "param"),
-        }
-    )),
+    ),
+    s({trig = "i", name = "New invoke", desc = "Create a new class invoke method"},
+        fmt(
+            [[
+                public function __invoke({} ${})
+                {{
+                }}
+            ]],
+            {
+                i(1, "hint"),
+                i(1, "param"),
+            }
+        )
+    ),
 
     -- unit test snippets
-    s("tm", fmt(
-        [[
-            /**
-             * @Given
-             * @When Call to
-             * @Then
-             */
-            public function test{}(): void
-            {{
-                // Given
+    s({trig = "tm", name = "New test method", desc = "Create a new unit test method"},
+      fmt(
+          [[
+              /**
+               * @Given
+               * @When Call to
+               * @Then
+               */
+              public function test{}(): void
+              {{
+                  // Given
 
-                // When
-                $result = $this->{}->{}(${});
+                  // When
+                  $result = $this->{}->{}(${});
 
-                // Then
-                self::assertTrue($result);
-            }}
-        ]],
-        {
-            i(1, "Name"),
-            i(2, "Service"),
-            i(3, "Method"),
-            i(4, "Param"),
-        }
-    )),
+                  // Then
+                  self::assertTrue($result);
+              }}
+          ]],
+          {
+              i(1, "Name"),
+              i(2, "Service"),
+              i(3, "Method"),
+              i(4, "Param"),
+          }
+      )
+    ),
     -- s("testset", fmt(
     --     [[
     --         private `!p snip.rv = uppercase_first(t[1])`|MockObject $${1};
@@ -172,36 +167,43 @@ ls.add_snippets("php", {
     --         i(4, "param"),
     --     }
     -- ))
-    s("tmk", fmt(
-        [[
-            $this->{} = $this->getMock({}::class);
-        ]],
-        {
-            i(1, "name"),
-            i(2, "Class"),
-        }
-    )),
-    s("tmkc", fmt(
-        [[
-            $this->{}->expects(self::once())
-            ->method('{}')
-            ->with({})
-            ->willReturn({});
-        ]],
-        {
-            i(1, "mock"),
-            i(2, "method"),
-            i(3, "params"),
-            i(4, "result"),
-        }
-    )),
-    s("tmp", fmt(
-        [[
-            private {}|MockObject ${};
-        ]],
-        {
-            i(1, "Class"),
-            i(2, "property"),
-        }
-    )),
+
+    s({trig = "tmk", name = "New test class mock", desc = "Create a new unit test class mock"},
+      fmt(
+          [[
+              $this->{} = $this->getMock({}::class);
+          ]],
+          {
+              i(1, "name"),
+              i(2, "Class"),
+          }
+      )
+    ),
+    s({trig = "tmkc", name = "New test mock class call", desc = "Create a new unit test mock class call"},
+      fmt(
+          [[
+              $this->{}->expects(self::once())
+              ->method('{}')
+              ->with({})
+              ->willReturn({});
+          ]],
+          {
+              i(1, "mock"),
+              i(2, "method"),
+              i(3, "params"),
+              i(4, "result"),
+          }
+      )
+    ),
+    s({trig = "tmkp", name = "New test class mock property", desc = "Create a new unit test class mock property"},
+      fmt(
+          [[
+              private {}|MockObject ${};
+          ]],
+          {
+              i(1, "Class"),
+              i(2, "property"),
+          }
+      )
+    ),
 })
