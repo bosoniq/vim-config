ac25071e420c6ddaf10cbb21d26b57d1ebefd52d
Andrew Pitt <bosoniq@gmail.com>
2025-01-01 02:15:44 +0100

    Phpstan level config removed

--- lua/modules/phpactor.lua
+++ lua/modules/phpactor.lua
@@ -1,7 +1,6 @@
 require'lspconfig'.phpactor.setup{
     on_attach = on_attach,
     init_options = {
-        ["phpstan.level"] = 9,
         ["language_server_phpstan.bin"] = phpstan,
         ["language_server_completion.trim_leading_dollar"] = true,
         ["logging.level"] =  "debug",
