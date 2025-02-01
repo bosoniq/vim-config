761ab6d968355abda83091ce9cf23043bc1da398
Andrew Pitt <bosoniq@gmail.com>
2025-02-01 22:27:52 +0100

    Claude support added

---
+++ lua/modules/claude.lua
@@ -0,0 +1,5 @@
+vim.g.claude_api_key = '<anthropic-console-api-key>'
+vim.g.claude_map_implement = "<Leader>ci"
+vim.g.claude_map_open_chat = "<Leader>cc"
+vim.g.claude_map_send_chat_message = "<C-]>"
+vim.g.claude_map_cancel_response = "<Leader>cx"
