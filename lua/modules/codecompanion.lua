c8be5c73af147bd7a19146afbc644a9d8d051a2e
Andrew Pitt <bosoniq@gmail.com>
2025-06-11 00:47:01 +0200

    Basic config for Gemini added

--- lua/modules/codecompanion.lua
+++ lua/modules/codecompanion.lua
@@ -1,1 +1,19 @@
-require("codecompanion").setup()
+require("codecompanion").setup({
+  strategies = {
+      chat = {
+        adapter = 'gemini',
+      },
+      inline = {
+        adapter = 'gemini',
+      },
+  },
+  adapters = {
+    gemini = function()
+      return require("codecompanion.adapters").extend("gemini", {
+        env = {
+          api_key = "GEMINI_ACCESS_TOKEN",
+        },
+      })
+    end,
+  },
+})
