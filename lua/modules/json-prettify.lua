0ab5fc6b3c0b836f2fe8c0f9cbf68255c7ad2bd1
Andrew Pitt <bosoniq@gmail.com>
2025-10-08 01:08:40 +0200

    Updated json beautify

--- lua/modules/json-prettify.lua
+++ lua/modules/json-prettify.lua
@@ -1,3 +1,17 @@
 -- Convert json to human readable
--- vim.keymap.set('n',  '=j', '<cmd>%!python -m json.tool<cr>')
-vim.keymap.set('n', '=j', function() os.execute('python3 -m json.tool') end)
+-- Convert json to human readable
+vim.keymap.set('n', '+j', function()
+  -- Save the current cursor position
+  local cursor_pos = vim.api.nvim_win_get_cursor(0)
+
+  -- Run the external formatter
+  vim.cmd('silent !npx prettier --write ' .. vim.fn.expand('%'))
+
+  -- Reload the buffer from the disk to see the changes
+  vim.cmd('edit!')
+
+  -- Restore the cursor position
+  vim.api.nvim_win_set_cursor(0, cursor_pos)
+
+  print('Formatted with Prettier')
+end, { desc = 'Format file with Prettier' })
